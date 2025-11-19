8faab501695082bd40d36b6b8cd7ff1be733d57b
OP-idk <ojparsley@gmail.com>
2025-11-19 14:47:47 -0800

    Finished bb84 algorithm

--- bb84.py
+++ bb84.py
@@ -5,26 +5,28 @@ from qiskit.visualization import plot_histogram
 import numpy as np
 from math import pi
 from enum import Enum
+import random as rng
 
-"""
-Method that takes in a quantum circuit, a basis, and a bit, and applies the 
-corresponding gates
+key_length = 32
+check_length = 4
 
-Method that takes in a quantum circuit and a basis, applies the corresponding
-gates, the measures and returns the recieved bit
-"""
+evesdropper = True
+
+a_bits = []
+a_bases = []
+a_key = []
+
+b_bits = []
+b_bases = []
+b_key = []
 
 qr = QuantumRegister(1)
 cr = ClassicalRegister(1)
 qc = QuantumCircuit(qr, cr)
-key_length = 16
 
 class Basis(Enum):
     ZERO = 1
     PLUS = 2
-
-for i in range(key_length):
-    pass
 
 def send(qc : QuantumCircuit, basis : Basis, bit : int ):
     if bit == 1:
@@ -48,17 +50,55 @@ def recieve(qc, basis : Basis) -> int:
         for c in counts: # take result of first shot then exit loop
             bit = int(c)
             break
-    #print(counts)
-    print(bit)
+    #print(bit)
     qc.reset(0)
-    #print(len(counts))
+    return bit
 
-    """
-    bit = expr.lift(cr[0])
-    with qc.if_test(bit):
-        print("1")
-    pass
-    """
+# Actual Algorithm
 for i in range(key_length):
-    send(qc, Basis.ZERO, 1)
-    recieve(qc, Basis.PLUS)
+    # Alice: Send
+    a_rand_basis = rng.choice([Basis.ZERO, Basis.PLUS])
+    a_rand_bit = rng.choice([0, 1])
+    
+    a_bases.append(a_rand_basis)
+    a_bits.append(a_rand_bit)
+
+    send(qc, a_rand_basis, a_rand_bit)
+
+    # Eve: Recieve and send
+    if evesdropper:
+        e_recieve_basis = rng.choice([Basis.ZERO, Basis.PLUS])
+        e_send_basis = rng.choice([Basis.ZERO, Basis.PLUS])
+        e_recieved_bit = recieve(qc, e_recieve_basis)
+        send(qc, e_send_basis, e_recieved_bit)
+    
+    b_recieve_basis = rng.choice([Basis.ZERO, Basis.PLUS])
+    b_recieved_bit = recieve(qc, b_recieve_basis)
+    
+    b_bases.append(b_recieve_basis)
+    b_bits.append(b_recieved_bit)
+
+print("Evesdropper present?: " + str(evesdropper))
+print("Alice's sent bits:   " + str(a_bits))
+print("Bob's recieved bits: " + str(b_bits))
+
+for i in range(key_length):
+    if a_bases[i] == b_bases[i]:
+        a_key.append(a_bits[i])
+        b_key.append(b_bits[i])
+
+print("")
+print("Alice's Key: " + str(a_key))
+print("Bob's key:   " + str(b_key))
+
+result_msg = "Key Valid!"
+# Check resulting key is a decent length
+if len(a_key) <= check_length * 2:
+    result_msg = "Key Invalid: too short"
+else:
+    # Check for evesdropper and discard check bits
+    for i in range(check_length):
+        if a_key[i] != b_key[i]:
+            result_msg = "Key Invalid: noise or evesdropper detected"
+
+print(result_msg)
